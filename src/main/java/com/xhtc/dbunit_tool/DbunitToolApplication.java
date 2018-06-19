a995159be988ca9e1faa76d7482417e756ec2458
rayeden <idealist.eden@gmail.com>
2018-06-19 18:05:53 +0800

    Initial commit

---
+++ src/main/java/com/xhtc/dbunit_tool/DbunitToolApplication.java
@@ -0,0 +1,12 @@
+package com.xhtc.dbunit_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbunitToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbunitToolApplication.class, args);
+    }
+}
